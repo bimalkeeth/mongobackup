fc3d9e94d5475004594092549e1b74dc158e5148
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2019-11-08 16:46:23 +1100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mongobackup
+
+go 1.13
+
+require github.com/aws/aws-sdk-go v1.15.77 // indirect
---
+++ mongomanager.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"strings"
+)
+
+func main() {
+
+	ses:=session.Must(session.NewSessionWithOptions(session.Options{
+		Config:                  aws.Config{Region:aws.String("ap-southeast-2")},
+		SharedConfigState:       session.SharedConfigEnable,
+	}))
+	cred:=credentials.NewStaticCredentials("AKIARWXENY46J4XV2HOS","9zDEqfJw+F6lGDda5uV0dvk9/MSr+x5mmVKnszhd","")
+	credent,err:=cred.Get()
+	if err!=nil{
+		fmt.Println(err)
+	}
+	fmt.Println("Value of credentials : ", credent)
+    svc:=ec2.New(ses,&aws.Config{Credentials:cred})
+
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("tag:Name"),
+				Values: []*string{
+					aws.String(strings.Join([]string{"*", ".", "*"}, "")),
+				},
+			},
+		},
+	}
+	resp, err := svc.DescribeInstances(params)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	fmt.Println(resp)
+
+
+}
